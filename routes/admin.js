13484e5f5dabb97a65002ce46467aea280d454c6
Ben Hutchison <ben@bluejeans.com>
2013-07-17 13:23:46 -0700

    Admin interface is persisting data

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,30 @@
 var personRepository = require('../lib/personRepository');
+var Q                = require('q');
 
 exports.index = function(req, res){
-	personRepository.getPeople()
+	personRepository.getAll()
 		.then(function(people){
   			res.render('admin', { people: people });
   		});
 };
 
 exports.save = function(req, res){
-	console.log(req.body);
-	res.redirect('.');
+	var peopleToInsert = req.body.people.map(function(rawPerson){
+		return {
+			name   : rawPerson.name,
+			period : rawPerson.period,
+			acv    : parseInt(rawPerson.acv) * 1000,
+			quota  : parseInt(rawPerson.quota) / 100
+		};
+	});
+
+	//remove existing people from collection
+	personRepository.deleteAll()
+		.then(function(){
+			return Q.all(peopleToInsert.map(personRepository.create));
+		})
+		.then(function(){
+			res.redirect('.?msg='+encodeURIComponent('Leaderboard updated successfully.'));
+		})
+		.done();
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var personRepository = require('../lib/personRepository');
 
 exports.index = function(req, res){
-	personRepository.getPeople()
+	personRepository.getAll()
 		.then(function(people){
 			res.render('index', { people: people });
 		});
