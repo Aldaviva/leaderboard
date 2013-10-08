0c7f0ba2382ad38b3fb0d1918cf241f7721dddd4
Ben Hutchison <ben@bluejeans.com>
2013-10-07 22:39:06 -0700

    Admin: wrong script url for typeahead

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var personRepository = require('../lib/personRepository');
 exports.index = function(req, res){
 	personRepository.getAll()
 		.then(function(categories){
-			console.log("categories:", categories);
 			res.render('index', { categories: categories });
 		});
 };
